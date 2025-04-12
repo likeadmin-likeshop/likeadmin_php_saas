8c9ba1d9ee39791ffd7d3ef705b460895773b7eb
dong-yifan9 <214290060@qq.com>
2025-04-12 22:49:45 +0800

    feat: 调整项目入口文件，当直接访问平台端域名时跳转至平台管理后台

--- server/public/index.php
+++ server/public/index.php
@@ -14,7 +14,15 @@ namespace think;
 
 require __DIR__ . '/../vendor/autoload.php';
 
+$app = new App();
+$host = $_SERVER['HTTP_HOST'];
+$env = loadEnv();
 $url = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+if ($url == '/' && $env['HTTP_HOST'] == $host) {
+    header("location:/platform/");
+    exit;
+}
+
 $whe = !str_starts_with($url, '/platformapi/config/getConfig') && !str_starts_with($url, '/tenantapi/config/getConfig') && !str_starts_with($url, '/api/pc/config') && !str_starts_with($url, '/api/index/config');
 if ($whe && !file_exists(__DIR__ . '/../config/install.lock')) {
     header("location:/install/install.php");
@@ -24,10 +32,48 @@ if ($whe && !file_exists(__DIR__ . '/../config/install.lock')) {
 define('ROOT_PATH', __DIR__);
 
 // 执行HTTP应用并响应
-$http = (new App())->http;
+$http = $app->http;
 
 $response = $http->run();
 
 $response->send();
 
 $http->end($response);
+
+
+function loadEnv(): array
+{
+
+    $lines = file(__DIR__ . '/../.env', FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+    $env = [];
+
+    foreach ($lines as $line) {
+        // 跳过注释行
+        if (str_starts_with(trim($line), '#')) {
+            continue;
+        }
+
+        // 跳过不包含=行
+        if (!str_contains($line, '=')) {
+            continue;
+        }
+
+        // 分割键值
+        list($key, $value) = explode('=', $line, 2);
+        $key = trim($key);
+        $value = trim($value);
+
+        // 移除值两端的引号
+        if (preg_match('/^"(.*)"$/', $value, $m) || preg_match('/^\'(.*)\'$/', $value, $m)) {
+            $value = $m[1];
+        }
+
+        // 处理转义字符（如\n）
+        $value = str_replace('\\n', "\n", $value);
+
+        // 存储到环境和全局变量
+        $env[$key] = $value;
+    }
+
+    return $env;
+}
